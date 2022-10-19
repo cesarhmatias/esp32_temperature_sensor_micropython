ec7849a035beeba15c339b2b4410745ed229b237
Cesar Henrique Mataran Matias <cesarhmmatias@gmail.com>
2022-10-19 02:07:23 -0300

    feature: added data parsing

--- main.py
+++ main.py
@@ -2,17 +2,49 @@ from machine import Pin, ADC
 from time import sleep
 import onewire
 import ds18x20
-from datetime import date
+# from datetime import date
+# import pandas as pd
 
 
 class Sensor:
     def __init__(self, pin):
         self.pin = Pin(pin)
-        self.date = date.today()
+        # self.date = date.today()
         self.data = None
+        # self.list_date = []
+        self.list_data = []
+        self.list_index = []
+        # self.df = pd.DataFrame()
+        self.dict = {
+                        'index':[],
+                        # 'date':[],
+                        'raw':[],
+                    }
 
-    def now(self):
-        self.date = date.today()
+    # def now(self):
+    # self.date = date.today()
+
+    def store_data(self, number, time):
+        for i in range(number):
+            self.list_index.append(i)
+            # self.list_date[i] = date.today()
+            self.list_data.append(self.read())
+            
+            sleep(time + 1)
+            
+        self.dict['index'] = self.list_index
+        # self.dict['date'] = self.list_date
+        self.dict['raw'] = self.list_data
+        return self.dict
+
+    # def parse_df(self):
+    #     self.df.index = self.list_index
+    #     # self.df['Date'] = self.date
+    #     self.df['raw'] = self.data
+    #     return self.df
+
+    def read(self):
+        raise NotImplemented()
 
 
 class DS18B20(Sensor):
@@ -35,27 +67,45 @@ class LM35(Sensor):
         Sensor.__init__(self, pin)
         self.adc = ADC(self.pin)
         self.adc.atten(ADC.ATTN_11DB)
+        self.schedule = 1000
+        self.total = 0
+        self.reading = 0
 
     def raw(self):
         self.reading = self.adc.read()
+        return self.reading
 
-    def temp_c(value):
+    def temp_c(self, value):
         return value / 10
 
     def read(self):
-        self.raw()
-        self.celsius_temp = self.temp_c(self.reading)
+        self.total = 0
+        self.reading = 0
+        self.total = 0
+        for i in range(self.schedule):
+            old = self.reading
+            self.raw()
+            self.total = old + self.total
+            #print(self.raw())
+            #print(self.reading)
+            #print(old)
+            #print(self.total)
+            #print(i)
+        #print(self.total)
+        self.celsius_temp = self.temp_c(self.total/self.schedule - 1)
+        
         return self.celsius_temp
-
 
     def fahrenheit(self):
         return (self.read() * (9 / 5)) + 32
 
 
-while True:
-    sensor_digital = DS18B20(26)
-    print(f'DS18B20: {sensor_digital.read()} graus.')
-    sensor_analogico = LM35(32)
-    print(f'LM35: {sensor_analogico.read()} graus.')
-    sleep(1)
+# while
+sensor_digital = DS18B20(26)
+print(f'DS18B20: {sensor_digital.read()} graus.')
+sensor_analogico = LM35(32)
+print(f'LM35: {sensor_analogico.read()} graus.')
+print(sensor_digital.store_data(5, 1))
+print(sensor_analogico.store_data(5, 1))
+
 
